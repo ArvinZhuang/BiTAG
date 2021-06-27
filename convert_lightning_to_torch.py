60bd0ef0c878259d0ebb85b8e24b2251e35bcf4c
ArvinZhuang <shengyaozhuang@gmail.com>
2021-06-28 08:19:35 +1000

    add requirements.txt

--- convert_lightning_to_torch.py
+++ convert_lightning_to_torch.py
@@ -1,7 +1,10 @@
 from train import absTtilePL
+from transformers import T5ForConditionalGeneration, T5Config
 
 checkpoint_path = "ckpts/gpu4_bs32_e2.ckpt"
 output_path = "ckpts/gpu4_bs32_e2"
 
-model = absTtilePL.load_from_checkpoint(checkpoint_path=checkpoint_path)
+config = T5Config.from_pretrained("t5-large", cache_dir=".cache", use_cache=True)
+t5 = T5ForConditionalGeneration.from_pretrained("t5-large", cache_dir=".cache", config=config)
+model = absTtilePL.load_from_checkpoint(checkpoint_path=checkpoint_path, model=t5)
 model.model.save_pretrained(output_path)
--- generate.py
+++ generate.py
@@ -1,13 +1,14 @@
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import T5ForConditionalGeneration, T5Tokenizer, T5Config
 import json
 
 if __name__ == "__main__":
     with open("generation_config.json") as f:
         config = json.loads(f.read())
 
-    model_name = 't5-small'
-    model = T5ForConditionalGeneration.from_pretrained(model_name, cache_dir=".cache")
-    tokenizer = T5Tokenizer.from_pretrained(model_name, cache_dir=".cache")
+    t5config = T5Config.from_pretrained("t5-large", cache_dir=".cache", use_cache=True)
+    model_name = "ckpts/gpu4_bs32_e2"
+    model = T5ForConditionalGeneration.from_pretrained(model_name, cache_dir=".cache", config=t5config)
+    tokenizer = T5Tokenizer.from_pretrained("t5-large", cache_dir=".cache")
 
     text = ""
     if config["task"] == "title_to_abs":
@@ -18,15 +19,28 @@ if __name__ == "__main__":
     print(text)
     input_ids = tokenizer.encode(text, return_tensors='pt')
 
-    beam_outputs = model.generate(
-        input_ids,
-        max_length=config["max_length"],
-        num_beams=config["num_beams"],
-        # no_repeat_ngram_size=2,
-        num_return_sequences=config["num_return_sequences"],
-        early_stopping=True
-    )
+    # beam_outputs = model.generate(
+    #     input_ids,
+    #     max_length=config["max_length"],
+    #     num_beams=config["num_beams"],
+    #     # no_repeat_ngram_size=2,
+    #     num_return_sequences=config["num_return_sequences"],
+    #     early_stopping=True
+    # )
+    #
+    # print("Output:\n" + 100 * '-')
+    # for i, beam_output in enumerate(beam_outputs):
+    #     print("{}: {}".format(i, tokenizer.decode(beam_output, skip_special_tokens=True)))
 
+    sample_outputs = model.generate(
+        input_ids,
+        do_sample=True,
+        max_length=500000,
+        top_p=0.95,
+        top_k=50,
+        num_return_sequences=10,
+        temperature=1.5
+    )
     print("Output:\n" + 100 * '-')
-    for i, beam_output in enumerate(beam_outputs):
-        print("{}: {}".format(i, tokenizer.decode(beam_output, skip_special_tokens=True)))
+    for i, sample_output in enumerate(sample_outputs):
+        print("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
